c740fc247027f92638589e239c38e3befdf89833
desmond.nnebue <sirdesmond09@gmail.com>
2024-03-05 13:27:46 +0100

    mod: extend views and models, fix constraint

---
+++ addons/product_addon/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Next Basket Product Extension',
+    'version': '16.0.1',
+    'depends': ['product'],
+    'author': 'Desmond Nnebue',
+    'description': 'Addon to extend basic Odoo functionalities',
+    'data': [
+        "views/product.xml"
+    ],
+    'installable': True,
+    'application': True,
+}
--- addons/product_addon/models/product.py
+++ addons/product_addon/models/product.py
@@ -1,25 +1,31 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-from odoo.tools import slugify
+from slugify import slugify
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     slug = fields.Char(string="Slug", compute="_compute_slug", store=True)
-    additional_barcode = fields.Char(string="Additional Barcode", unique=True)
+    additional_barcode = fields.Char(string="Additional Barcode")
 
     @api.depends("name")
     def _compute_slug(self):
         for record in self:
-            record.slug = slugify(record.name)
+            if record.name:
+                record["slug"] = slugify(str(record.name))
+            else:
+                record["slug"] = ""
 
     @api.constrains("additional_barcode")
     def _check_additional_barcode_unique(self):
         for record in self:
-            if record.additional_barcode:
+            if record["additional_barcode"]:
                 existing_product = self.env["product.template"].search(
-                    [("additional_barcode", "=", record.additional_barcode)]
+                    [
+                        ("additional_barcode", "=", record.additional_barcode),
+                        ("id", "!=", record.id),
+                    ]
                 )
                 if existing_product:
                     raise ValidationError("Additional barcode must be unique.")
